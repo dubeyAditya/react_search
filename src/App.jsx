a44de193908551e5842c6f897a8517c6d9508a12
Aditya Dubey <sinister.dubey@gmail.com>
2020-03-23 17:07:58 +0530

    added first react component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Style, { AppWrpper } from './styled';
+const App = () => {
+    return (
+        <AppWrpper>
+            <div> Hello React App </div>
+            <Style/>
+        </AppWrpper>)
+}
+
+export default App;
---
+++ src/styled.js
@@ -0,0 +1,15 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const AppWrpper = styled.div`
+  height: 100%;
+  display: flex;
+  padding: 0.25 rem;
+  border: 1px solid #f2f2f2;
+`
+const Style = createGlobalStyle`
+ body, #root {
+   height: 97vh;
+}
+`
+export default Style;
+
