8ae7f53b6e0ee284227856fc66a8b19f37127b5b
Aditya Dubey <sinister.dubey@gmail.com>
2020-03-25 14:04:07 +0530

    Added TabFactory

--- src/common/TabList/TabList.jsx
+++ src/common/TabList/TabList.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import { Test } from './TabList.styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlane,faCar, faHotel } from '@fortawesome/free-solid-svg-icons';
+import { TabListWrapper,Tab } from './TabList.styles';
 
-const TabList = (props) => (
-  <div className="TabListWrapper">
-    Test content
+
+const TabList = ({selected,onChange}) => {
+  
+  const handleTabChange = (tab) => () => {
+     onChange(tab);
+  } 
+
+  return <div className="TabListWrapper">
+      <TabListWrapper>
+        <Tab onClick={handleTabChange(1)} selected={selected === 1}>
+          <span><FontAwesomeIcon icon={faPlane} rotation={270} /></span>
+          <span>Flights</span>
+        </Tab>
+        <Tab onClick={handleTabChange(2)}  selected={selected === 2}>
+          <span><FontAwesomeIcon icon={faHotel} /></span>
+          <span>Hotels</span>
+        </Tab>
+        <Tab onClick={handleTabChange(3)}  selected={selected === 3}>
+        <span><FontAwesomeIcon icon={faCar} /></span>
+          <span>Cars</span>
+        </Tab>
+      </TabListWrapper>
   </div>
-);
+};
 
 TabList.propTypes = {
-  // bla: PropTypes.string,
+  selected: PropTypes.number,
+  onChange: PropTypes.func
 };
 
 TabList.defaultProps = {
--- src/common/TabList/TabList.styles.js
+++ src/common/TabList/TabList.styles.js
@@ -1,6 +1,19 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// export const Test = styled.div`
-//  display: flex;
-// `;
-//
+export const Tab = styled.div`
+   border-bottom: ${prop => prop.selected ? '2px solid #0159A1': '2px solid gray' };
+   color: ${prop => prop.selected ? '#2CB3EE': 'gray' };
+   text-align: center;
+   padding: 0.5rem;
+   cursor: pointer;
+   & :nth-child(2){
+     margin-left: 0.5rem;
+   }
+`;
+
+export const TabListWrapper = styled.div`
+   display: grid;
+   grid-template-columns: repeat(3, 1fr);
+   grid-gap: 5px; 
+   width:100%;
+` 
