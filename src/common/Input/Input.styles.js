2c0141c197d300d554fbbc57e4a0a3816e5fe0c8
Aditya Dubey <sinister.dubey@gmail.com>
2020-03-26 02:26:00 +0530

    Input Issue with Chorme

--- src/common/Input/Input.styles.js
+++ src/common/Input/Input.styles.js
@@ -2,7 +2,13 @@ import styled from 'styled-components';
 
 export const InputWrapper = styled.input`
   padding:0.75rem;
+  width: -webkit-fill-available;
   width: -moz-available;
   border: 1px solid #d9d9d9;
   border-radius: 4px;
+
+  & :focus{
+    border-color: #B9F3FF;
+  }
+
 `;
