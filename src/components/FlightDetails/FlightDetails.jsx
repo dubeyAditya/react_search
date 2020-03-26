97e69460786cbc79f2da1d1bd06326bf7b87ed08
Aditya Dubey <sinister.dubey@gmail.com>
2020-03-26 18:12:42 +0530

    Implemented Flight Card

--- src/components/FlightDetails/FlightDetails.jsx
+++ src/components/FlightDetails/FlightDetails.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FlightCard, Title } from './FlightDetails.styles';
+import { FlightCard, Title, Content, Footer, ClassBox, DepartureArrivalBox } from './FlightDetails.styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
 
-const FlightDetails = (props) => (
-  <FlightCard>
-   <Title>
-     <div><FontAwesomeIcon icon={faPlaneDeparture}/> </div>
-     <div> {props.flight.name} </div>
-   </Title>
-   
-  </FlightCard>
-);
+const FlightDetails = ({ flight }) => {
+
+  const { name, departs, arrives, flightClass , origin, dest } = flight;
+
+
+  const renderClassBox = (item) => (
+      <ClassBox>
+        <div>{`$${item.fare}`}</div>
+        <div>{item.title}</div>
+     </ClassBox>
+  )
+
+  return (<FlightCard>
+    <Title>
+      <div><FontAwesomeIcon icon={faPlaneDeparture} /> </div>
+      <div> {name} </div>
+    </Title>
+    
+    <Content>
+      <DepartureArrivalBox>
+        <div>{departs}</div>
+        <div>{origin}</div>
+      </DepartureArrivalBox>
+      <DepartureArrivalBox>
+        <div>{arrives}</div>
+        <div>{dest}</div>
+      </DepartureArrivalBox>
+    </Content>
+
+    <Footer>
+     {flightClass.map(renderClassBox)}
+    </Footer>
+  </FlightCard>)
+};
 
 FlightDetails.propTypes = {
   flight: PropTypes.object,
--- src/components/FlightDetails/FlightDetails.styles.js
+++ src/components/FlightDetails/FlightDetails.styles.js
@@ -6,8 +6,13 @@ export const Flex = styled.div`
 
 export const FlightCard = styled(Flex)`
   padding: 1rem;
+  flex-direction:column;
   background: white;
   border-bottom: 2px solid #e8e8e8;
+
+  & > div {
+    margin-bottom:0.5rem;
+  }
 `;
 
 export const Title = styled(Flex)`
@@ -26,5 +31,43 @@ export const Title = styled(Flex)`
 
 export const Content = styled(Flex)`
  justify-content: space-between;
+
+ & > div:last-child{
+   text-align-last:right;
+ } 
+
+ & > div{
+   flex:1;
+ }
  
+`
+
+export const DepartureArrivalBox = styled(Flex)`
+   flex-direction: column; 
+   
+  & :nth-child(1){
+    font-size: 1rem;
+  }
+
+  & :nth-child(2){
+    font-size: 0.75rem;
+  }
+`
+
+export const ClassBox = styled(DepartureArrivalBox)`
+  border: 1px solid #0159A1;
+  background-color:#0159A1;
+  color: #ffff;
+  padding: 0.25rem;
+`
+
+
+export const Footer = styled(Flex)`
+  justify-content:flex-start;
+  overflow-y:scroll;
+ 
+  & > div{
+    margin-right: 0.25rem;
+    flex-basis: 5rem; 
+  }
 `
